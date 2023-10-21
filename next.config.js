782bd266fdb5a36b2bd7964786e7eb5275f1dca2
Andrew Brainerd <drwb333@gmail.com>
2023-10-21 01:50:42 -0400

    Update packages

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
   },
