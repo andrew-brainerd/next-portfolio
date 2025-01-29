cadb177b257653ad29ba32eabfa1c897d9e97b85
Andrew Brainerd <drwb333@gmail.com>
2025-01-29 12:04:13 -0500

    Add Crisis Core to completed

--- src/constants/steam.ts
+++ src/constants/steam.ts
@@ -63,4 +63,5 @@ export const COMPLETED_GAMES: string[] = [
   '990080',    // Hogwarts Legacy
   '2322010',   // God of War Ragnarok
   '2515020',   // Final Fantasy XVI
+  '1608070',   // CRISIS CORE FINAL FANTASY VII REUNION
 ];
