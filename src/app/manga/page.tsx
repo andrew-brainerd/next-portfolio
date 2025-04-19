3c5fc81f5ded0fed443a5ab47b43bf4a6d9c210b
Andrew Brainerd <drwb333@gmail.com>
2025-04-19 09:22:42 -0400

    Update packages

--- src/app/manga/page.tsx
+++ src/app/manga/page.tsx
@@ -38,6 +38,8 @@ export default function MangePage() {
 
     const searchData = await postRequest<MangePostData, Manga[] | string>('/manga/search', { searchTerm: term });
 
+    console.log('Search Data', searchData);
+
     if (typeof searchData === 'string') {
       setSearchPage(searchData);
     } else {
