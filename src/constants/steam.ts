db7e83dcbcc6ffc083411a33a25e8b249f12e94b
Andrew Brainerd <drwb333@gmail.com>
2025-08-02 19:45:39 -0400

    Add DOOM The Dark Ages to completed games

--- src/constants/steam.ts
+++ src/constants/steam.ts
@@ -5,64 +5,65 @@ export const MY_STEAM_ID = '76561197998637344';
 export const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
 export const COMPLETED_GAMES: string[] = [
-  '32440',     // LEGO Star Wars: The Complete Saga
-  '368260',    // Marvel's Midnight Suns
-  '648800',    // Raft
-  '21130',     // LEGO Harry Potter: Years 1-4
-  '1817070',   // Marvel's Spider-Man Remastered
-  '648800',    // Raft
-  '1003590',   // Tetris Effect: Connected
-  '218060',    // BIT.TRIP Runner2: Future Legend of Rhythm Alien,
-  '21130',     // LEGO Harry Potter: Years 1-4
-  '1817070',   // Marvel’s Spider-Man Remastered
-  '241930',    // Middle-earth: Shadow of Mordor
-  '578330',    // LEGO City Undercover
-  '1593500',   // God of War
-  '924980',    // Trials of Mana
-  '311770',    // LEGO Pirates of the Caribbean The Video Game,
-  '1240440',   // Halo Infinite
-  '742420',    // Saints Row
-  '1599020',   // Tinykin
-  '607080',    // Psychonauts 2
-  '225540',    // Just Cause 3
-  '1088850',   // Marvel's Guardians of the Galaxy
-  '1599660',   // Sackboy: A Big Adventure
-  '1426210',   // It Takes Two3540
-  '3830',      // Psychonauts
-  '1065310',   // Evil West
-  '1895880',   // Ratchet & Clank: Rift Apart
-  '213670',    // South Park: The Stick of Truth
-  '3590',      // Plants vs. Zombies
-  '737520',    // Flynn: Son of Crimson
-  '1282150',   // SpongeBob SquarePants: The Cosmic Shake
-  '1332010',   // Stray
-  '400',       // Portal
-  '383870',    // Firewatch
-  '1038300',   // New Super Lucky's Tale
-  '1238080',   // Burnout Paradise Remastered
-  '1823930',   // Wavetale
-  '865360',    // We Were Here Together
-  '677160',    // We Were Here Too
-  '501300',    // What Remains of Edith Finch
-  '582500',    // We Were Here
-  '3540',      // Peggle Nights
-  '1817190',   // Marvel's Spider-Man: Miles Morales
-  '1687950',   // Persona 5 Royal
-  '593280',    // Cat Quest
-  '2222540',   // BIT.TRIP RERUNNER
-  '1237970',   // Titanfall 2
-  '2161700',   // Persona 3: Reload
-  '1244090',   // Sea of Stars
-  '2316580',   // Tales of Kenzera: ZAU
-  '881020',    // Granblue Fantasy Relink
-  '1290000',   // PowerWash Simulator
-  '1681430',   // RoboCop: Rogue City
-  '2567870',   // Chained Together
-  '1341290',   // We Were Here Forever
-  '396710',    // The Adventure Pals
-  '990080',    // Hogwarts Legacy
-  '2322010',   // God of War Ragnarok
-  '2515020',   // Final Fantasy XVI
-  '1608070',   // CRISIS CORE FINAL FANTASY VII REUNION
-  '1462040',   // FINAL FANTASY VII REMAKE INTERGRADE
+  '32440', // LEGO Star Wars: The Complete Saga
+  '368260', // Marvel's Midnight Suns
+  '648800', // Raft
+  '21130', // LEGO Harry Potter: Years 1-4
+  '1817070', // Marvel's Spider-Man Remastered
+  '648800', // Raft
+  '1003590', // Tetris Effect: Connected
+  '218060', // BIT.TRIP Runner2: Future Legend of Rhythm Alien,
+  '21130', // LEGO Harry Potter: Years 1-4
+  '1817070', // Marvel’s Spider-Man Remastered
+  '241930', // Middle-earth: Shadow of Mordor
+  '578330', // LEGO City Undercover
+  '1593500', // God of War
+  '924980', // Trials of Mana
+  '311770', // LEGO Pirates of the Caribbean The Video Game,
+  '1240440', // Halo Infinite
+  '742420', // Saints Row
+  '1599020', // Tinykin
+  '607080', // Psychonauts 2
+  '225540', // Just Cause 3
+  '1088850', // Marvel's Guardians of the Galaxy
+  '1599660', // Sackboy: A Big Adventure
+  '1426210', // It Takes Two3540
+  '3830', // Psychonauts
+  '1065310', // Evil West
+  '1895880', // Ratchet & Clank: Rift Apart
+  '213670', // South Park: The Stick of Truth
+  '3590', // Plants vs. Zombies
+  '737520', // Flynn: Son of Crimson
+  '1282150', // SpongeBob SquarePants: The Cosmic Shake
+  '1332010', // Stray
+  '400', // Portal
+  '383870', // Firewatch
+  '1038300', // New Super Lucky's Tale
+  '1238080', // Burnout Paradise Remastered
+  '1823930', // Wavetale
+  '865360', // We Were Here Together
+  '677160', // We Were Here Too
+  '501300', // What Remains of Edith Finch
+  '582500', // We Were Here
+  '3540', // Peggle Nights
+  '1817190', // Marvel's Spider-Man: Miles Morales
+  '1687950', // Persona 5 Royal
+  '593280', // Cat Quest
+  '2222540', // BIT.TRIP RERUNNER
+  '1237970', // Titanfall 2
+  '2161700', // Persona 3: Reload
+  '1244090', // Sea of Stars
+  '2316580', // Tales of Kenzera: ZAU
+  '881020', // Granblue Fantasy Relink
+  '1290000', // PowerWash Simulator
+  '1681430', // RoboCop: Rogue City
+  '2567870', // Chained Together
+  '1341290', // We Were Here Forever
+  '396710', // The Adventure Pals
+  '990080', // Hogwarts Legacy
+  '2322010', // God of War Ragnarok
+  '2515020', // Final Fantasy XVI
+  '1608070', // CRISIS CORE FINAL FANTASY VII REUNION
+  '1462040', // FINAL FANTASY VII REMAKE INTERGRADE
+  '3017860' // DOOM The Dark Ages
 ];
