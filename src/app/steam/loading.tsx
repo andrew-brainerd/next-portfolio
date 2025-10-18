ea6f707b5a166724747ead35cac7304df01de55b
Andrew Brainerd <drwb333@gmail.com>
2025-10-17 22:46:44 -0400

    Remove Steam loading component

--- src/app/steam/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Loading from 'components/Loading';
-
-export default function SteamLoading() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Loading />
-    </div>
-  );
-}
