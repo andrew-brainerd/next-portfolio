7a590fece5ca7eb901d05496c5b977a1e072f780
Andrew Brainerd <drwb333@gmail.com>
2026-04-03 14:54:26 -0700

    Show track name while paused

--- src/components/peapod/OwnerPlayer.tsx
+++ src/components/peapod/OwnerPlayer.tsx
@@ -16,7 +16,7 @@ interface OwnerPlayerProps {
 export default function OwnerPlayer({ isPlaying, trackName, nowPlaying, albumArt, onPlay, onPause }: OwnerPlayerProps) {
   return (
     <div className="bg-neutral-800 rounded-xl text-base mx-auto my-4 max-w-3xl overflow-hidden w-[95%]">
-      {isPlaying ? (
+      {trackName ? (
         <div className="flex items-center border-b-4 border-brand-500 text-2xl p-6 relative">
           <div className="flex-[3]">
             <div className="text-3xl mb-6">{trackName}</div>
@@ -26,7 +26,7 @@ export default function OwnerPlayer({ isPlaying, trackName, nowPlaying, albumArt
           <div className="flex-1">{albumArt && <img className="w-24" src={albumArt} alt="Album Art" />}</div>
         </div>
       ) : (
-        <div className="flex items-center border-b-4 border-brand-500 text-5xl h-36 justify-center">
+        <div className="flex items-center border-b-4 border-brand-500 h-36 justify-center">
           <Controls isPlaying={isPlaying} onPlay={onPlay} onPause={onPause} />
         </div>
       )}
