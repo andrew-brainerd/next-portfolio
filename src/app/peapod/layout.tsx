5bedbae05b12a6bcd2e1c70d780ed54a9b5aa81d
Andrew Brainerd <drwb333@gmail.com>
2026-04-03 10:21:40 -0700

    Add logo to login screen

--- src/app/peapod/layout.tsx
+++ src/app/peapod/layout.tsx
@@ -1,6 +1,8 @@
+import Image from 'next/image';
 import { cookies } from 'next/headers';
 import { TOKEN_COOKIE } from '@/constants/authentication';
 import PeapodNotification from '@/components/peapod/Notification';
+import peapodLogo from '@/img/peapod-logo.png';
 
 export const metadata = {
   title: 'Peapod'
@@ -12,8 +14,9 @@ export default async function PeapodLayout({ children }: { children: React.React
 
   if (!token) {
     return (
-      <div className="flex items-center justify-center h-screen">
-        <p className="text-white text-xl">Please log in to use Peapod.</p>
+      <div className="flex flex-col items-center justify-center h-[calc(100vh-140px)]">
+        <Image src={peapodLogo} alt="Peapod" width={96} height={96} className="mb-4" />
+        <p className="text-white text-xl">Please log in to use Peapod</p>
       </div>
     );
   }
